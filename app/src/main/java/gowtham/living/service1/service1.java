a633180f3c241ed7f22941a8f587cfb2eda125de
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-03 22:23:38 +0530

    logger removed

--- app/src/main/java/gowtham/living/service1/service1.java
+++ app/src/main/java/gowtham/living/service1/service1.java
@@ -1,19 +1,16 @@
 package gowtham.living.service1;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class service1 {
     private static final String API_KEY = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJ1c2VyIjoidGVzdC";
-    private static final Logger logger = LoggerFactory.getLogger(service1.class);
+
     @GetMapping("/name")
     public String printPodName() {
-        logger.info("name method called");
         return System.getenv("POD_NAME");
     }
     @GetMapping("/secret")
     public String printSecret() {
-        logger.info("secret method called");
         return API_KEY;
     }
 }
