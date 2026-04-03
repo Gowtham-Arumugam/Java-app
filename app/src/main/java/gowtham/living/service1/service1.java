dc935fb72df07f7d57cb141aeb96d0e92f9e4785
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-03 22:12:46 +0530

    sdfs

--- app/src/main/java/gowtham/living/service1/service1.java
+++ app/src/main/java/gowtham/living/service1/service1.java
@@ -3,23 +3,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class service1 {
-    @GetMapping("/service")
-    public String hello() {
-        return "Hello from Service 1";
-    }
-    @GetMapping("/greet")
-    public String greet() {
-        return "Greetings from Service 1 !!!!";
-    }
-
-    
-    @GetMapping("/status")
-    public String status() {
-        return "Service 1 is running smoothly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-    } 
-    @GetMapping("/")
+    private static final String API_KEY = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJ1c2VyIjoidGVzdC";
+    @GetMapping("/name")
     public String printPodName() {
-        System.out.println("Root method called");
+        System.out.println("name method called");
         return System.getenv("POD_NAME");
+    }
+    @GetMapping("/secret")
+    public String printSecret() {
+        System.out.println("Secret method called");
+        return API_KEY;
     }
 }
