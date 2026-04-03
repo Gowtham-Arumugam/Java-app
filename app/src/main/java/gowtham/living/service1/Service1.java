3aad7154238c29effc8e323aca6092fd4cc73398
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-03 22:28:59 +0530

    fdsfd

---
+++ app/src/main/java/gowtham/living/service1/Service1.java
@@ -0,0 +1,24 @@
+package gowtham.living.service1;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Service1 {
+    private static final Logger logger = LoggerFactory.getLogger(Service1.class);
+    @GetMapping("/name")
+    public String printPodName() {
+        logger.info("name method called");
+        return System.getenv("POD_NAME");
+    }
+    @GetMapping("/version")
+    public String printVersion() {
+        logger.info("version method called");
+        return System.getenv("VERSION");
+    }   
+    @GetMapping("/color")
+    public String printColor() {
+        logger.info("color method called");
+        return System.getenv("COLOR");
+    }
+}
