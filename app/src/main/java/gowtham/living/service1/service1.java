0af9e1705abce9a60c24adacdc967693858902fa
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-03 22:21:20 +0530

    werw

--- app/src/main/java/gowtham/living/service1/service1.java
+++ app/src/main/java/gowtham/living/service1/service1.java
@@ -1,17 +1,19 @@
 package gowtham.living.service1;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class service1 {
     private static final String API_KEY = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJ1c2VyIjoidGVzdC";
+    private static final Logger logger = LoggerFactory.getLogger(service1.class);
     @GetMapping("/name")
     public String printPodName() {
-        System.out.println("name method called");
+        logger.info("name method called");
         return System.getenv("POD_NAME");
     }
     @GetMapping("/secret")
     public String printSecret() {
-        System.out.println("Secret method called");
+        logger.info("secret method called");
         return API_KEY;
     }
 }
