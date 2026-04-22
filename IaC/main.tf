9ce12713dc3c14b515d82c062badbac7b45ea5e5
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-22 15:13:31 +0530

    working in local

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,20 +1,3 @@
-terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 3.0"
-        }
-    }
-}
-
-provider "azurerm" {
-    features {}
-    client_id       = var.CLIENT_ID
-    client_secret   = var.SECRET
-    tenant_id       = var.TENANT_ID
-    subscription_id = var.Subscription_ID
-}
-
 resource "azurerm_resource_group" "main" {
     name     = "rg-main"
     location = "East US"
