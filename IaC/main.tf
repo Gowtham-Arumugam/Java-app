b4cd06bd55b2979301ed2eedd3fa35c196fa2b22
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-22 15:46:37 +0530

    again changing rm name

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "three-tier-app-rg" {
-    name     = "ThreeTierApp_RG"
+    name     = "ThreeTierApp-RG"
     location = "East US"
     tags = {
         environment = "production"
