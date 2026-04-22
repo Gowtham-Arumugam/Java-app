d24d343d48027c406ee4a848f3e18d78791de235
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-22 14:29:11 +0530

    variable added

--- IaC/main.tf
+++ IaC/main.tf
@@ -8,7 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-    features {}
+    client_id     = var.CLIENT_ID
+    client_secret = var.SECRET
+    tenant_id     = var.TENANT_ID
+    subscription_id = var.Subscription_ID
 }
 
 resource "azurerm_resource_group" "main" {
---
+++ IaC/variables.tf
@@ -0,0 +1,16 @@
+variable "SECRET" {
+  description = "The client secret for authentication."
+  type        = string
+}
+variable "TENANT_ID" {
+  description = "The tenant ID for authentication."
+  type        = string
+}
+variable "Subscription_ID" {
+  description = "The subscription ID for authentication."
+  type        = string
+}
+variable "CLIENT_ID" {
+  description = "The client ID for authentication."
+  type        = string
+}
