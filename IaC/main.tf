cfb16a5d90c5f157de6266238e19cb379d499fc2
GowthamArumugam <gowthamcruise@gmail.com>
2026-04-23 13:51:42 +0530

    cost optimizted

--- IaC/main.tf
+++ IaC/main.tf
@@ -15,11 +15,16 @@ resource "azurerm_kubernetes_cluster" "three-tier-app-aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_B1s"
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "basic"
   }
 
   tags = {
